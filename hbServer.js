91d415b259bdb12a4cea49fa7f867291e807f761
paul <corpr8@hotmail.com>
2014-09-06 17:28:57 +0100

    cut across to handlebars

---
+++ hbServer.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var expressHbs = require('express3-handlebars');
+
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
+
+console.log("__dirname: " + __dirname);
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+/*
+app.get('/simple', function(req, res){
+  var data = {name: 'Gorilla'};
+  res.render('simple', data);
+});
+
+app.get('/complex', function(req, res){
+  var data = {
+    name: 'Gorilla',
+    address: {
+      streetName: 'Broadway',
+      streetNumber: '721',
+      floor: 4,
+      addressType: {
+        typeName: 'residential'
+      }
+    }
+  };
+  res.render('complex', data);
+});
+
+app.get('/loop', function(req, res){
+  var basketballPlayers = [
+    {name: 'Lebron James', team: 'the Heat'},
+    {name: 'Kevin Durant', team: 'the Thunder'},
+    {name: 'Kobe Jordan',  team: 'the Lakers'}
+  ];
+  
+  var days = [
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+  ];
+  
+  var data = {
+    basketballPlayers: basketballPlayers,
+    days: days
+  };
+  
+  res.render('loop', data);
+});
+
+app.get('/logic', function(req, res){
+  var data = {
+    upIsUp: true,
+    downIsUp: false,
+    skyIsBlue: "yes"
+  };
+  
+  res.render('logic', data);
+});
+*/
+
+//app.use(express.static( __dirname + '/stylesheets'));
+app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/js', express.static(__dirname + '/js'));
+app.listen(3001);
